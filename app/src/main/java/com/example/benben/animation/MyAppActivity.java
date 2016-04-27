7da3b7cdb52dbcc438d3e743d36befcab50884cd
junjie <1287550045@qq.com>
2016-04-27 11:24:37 +0800

    测试

---
+++ app/src/main/java/com/example/benben/animation/MyAppActivity.java
@@ -0,0 +1,9 @@
+package com.example.benben.animation;
+
+import com.example.benben.animation.ui.activity.BaseActivity;
+
+/**
+ * Created by tangjunjie on 2016/4/27.
+ */
+public class MyAppActivity extends BaseActivity {
+}
